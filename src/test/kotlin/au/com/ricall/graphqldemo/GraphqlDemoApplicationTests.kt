6447bebc2b9d11566c78d23f25573c3f43f1b5de
Richard Allwood <richard.allwood@circlein.com>
2022-04-15 12:53:29 +1000

    Initial Commit

---
+++ src/test/kotlin/au/com/ricall/graphqldemo/GraphqlDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package au.com.ricall.graphqldemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GraphqlDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
